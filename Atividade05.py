0ac9981666c336f9c0f18887309a7374073aabfb
GustavoWendt <gustavowendt14@gmail.com>
2025-02-07 19:58:45 -0300

    Atividades até a 8.
    Atividades até a 8.

---
+++ Atividade05.py
@@ -0,0 +1,3 @@
+#Faça um programa que solicite o nome do usuário e exiba a mensagem "Olá + nome digitado
+nome=input("Digite o seu nome:")
+print("Olá!",nome)
---
+++ Atividade06.py
@@ -0,0 +1,4 @@
+#Faça um programa que solicite o nome e em seguida solicite o sobre nome do usuário e exiba a mensagem "Olá" + nome digitado + sobre nome digitado.
+nome=input("Digite o seu nome:")
+sobrenome=input("Digite o seu sobrenome:")
+print("Olá!",nome,sobrenome)
