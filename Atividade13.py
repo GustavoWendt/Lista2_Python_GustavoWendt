1dc4ef66c5cd6e52988261cccc026751df45f4bb
GustavoWendt <gustavowendt14@gmail.com>
2025-02-07 21:12:09 -0300

    Atividades até a 13.
    Atividades até a 13.

---
+++ Atividade13.py
@@ -0,0 +1,6 @@
+#Escreva um programa que solicite um determinado números de dias, em seguida exiba o quanto esses dias equivalem em horas, minutos e segundos
+dias=float(input("Quantos dias?:"))
+horas=dias*24
+minutos=horas*60
+segundos=minutos*60
+print("horas:",horas,"minutos:",minutos,"segundos:", segundos)
