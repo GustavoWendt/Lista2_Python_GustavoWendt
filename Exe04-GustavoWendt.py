0f7dc42ca4b3069136aa398190f79ff1178d496f
GustavoWendt <gustavowendt14@gmail.com>
2025-02-07 21:31:28 -0300

    Atividades da 1 a 13.
    Atividades da 1 a 13.

---
+++ Exe04-GustavoWendt.py
@@ -0,0 +1,5 @@
+#Faça um prgrama que calcule um almento de 15% para um salário de R$750.
+Salario=750
+aumento=15
+Salariofinal=Salario+(Salario*aumento/100)
+print ("Seu salário com o aumento é de:",Salariofinal)
